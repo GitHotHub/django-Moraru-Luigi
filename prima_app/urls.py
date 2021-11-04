1720959c0a445e695af2e4a36c6a693fd746760b
GitHotHub <gigihitech@gmail.com>
2021-11-04 10:58:09 +0100

    Livello 4

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -1,6 +1,7 @@
 from os import name
 from django.urls import path
 from prima_app.views import homepage,welcome,lista,chi_siamo
+from prima_app.views import variabili
 
 app_name="prima_app"
 urlpatterns=[
@@ -8,4 +9,5 @@ urlpatterns=[
     path('welcome',welcome,name='welcome'),
     path('lista', lista ,name='lista'),
     path('chi_siamo',chi_siamo , name="chi_siamo"),
+    path('variabili',variabili,name="variabili"),
 ]
