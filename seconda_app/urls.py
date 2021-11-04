f0f4e653a08caf2c992dd9765b3f690a5a1b376f
GitHotHub <gigihitech@gmail.com>
2021-11-04 11:31:20 +0100

    Creata seconda app

---
+++ seconda_app/urls.py
@@ -0,0 +1,7 @@
+from os import name
+from django.urls import path
+from seconda_app.views import homepage,welcome,lista,chi_siamo,index
+from seconda_app.views import variabili
+
+app_name="secodna_app"
+urlpatterns=[]
